7feb91d5d6d16fd8cd58b868ec027cd8d977a36d
Joao Oliveira <joaoliveira1990@gmail.com>
2022-07-28 01:51:40 +0100

    fixed tailwind hover on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,17 @@ import Link from "next/link"
 
 function Navbar() {
     return (
-        <nav className="relative w-full flex flex-wrap items-center py-4 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
+        <nav className="relative w-full flex flex-wrap items-center py-4 bg-gray-100 shadow-lg">
             <div className="w-full flex flex-wrap flex-row items-center px-6">
                 <ul className="flex flex-grow flex-row pl-0 list-none w-auto justify-center">
                     <li className="p-2">
                         <Link className="" href="/">Home</Link>
                     </li>
-                    <li className="p-2">
-                        <Link className="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/">Features</Link>
+                    <li className="text-gray-500 hover:text-red-500 focus:text-gray-700 p-2">
+                        <Link className="" href="/">Blog</Link>
                     </li>
-                    <li className="p-2">
-                        <Link className="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/">Pricing</Link>
+                    <li className="text-gray-500 hover:text-red-500 focus:text-gray-700 p-2">
+                        <Link className="" href="/">About</Link>
                     </li>
                 </ul>
             </div>
