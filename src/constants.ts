5de7269af41983873474b1b1632a84663b7cb369
Joao Oliveira <joaoliveira1990@gmail.com>
2022-07-30 01:43:26 +0100

    added vercel env url in constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+export const baseUrl = process.env.VERCEL_URL
     ? `${process.env.NEXT_PUBLIC_VERCEL_URL}:3000`
     : 'http://localhost:3000'
 
